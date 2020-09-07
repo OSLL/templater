e3a80eb03479a625eedfcba98832a0ac7087b125
Nguyen Quang Huy <nguyenquanghuy1997@gmail.com>
2020-09-07 19:23:51 +0300

    little edit

--- build_and_run.sh
+++ build_and_run.sh
@@ -3,8 +3,8 @@
 RECAPTCHA_SITE_KEY=
 RECAPTCHA_SECRET_KEY=
 
-sed -i "s/let recaptcha_site_key = RECAPTCHA_SITE_KEY/let recaptcha_site_key = $RECAPTCHA_SITE_KEY/" app/templater/static/js/main.js
-sed -i "s/recaptcha_key = RECAPTCHA_SECRET_KEY/recaptcha_key = $RECAPTCHA_SECRET_KEY/" app/config.ini
+sed -i "s/let recaptcha_site_key = .*/let recaptcha_site_key = $RECAPTCHA_SITE_KEY/" app/templater/static/js/main.js
+sed -i "s/recaptcha_key = .*/recaptcha_key = $RECAPTCHA_SECRET_KEY/" app/config.ini
 
 
 docker-compose build --no-cache 
