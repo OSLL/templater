a141aadbbaaa31f528389dc9179248316069d1c9
Nguyen Quang Huy <nguyenquanghuy1997@gmail.com>
2020-09-08 22:35:42 +0300

    fixed a bug

--- build_and_run.sh
+++ build_and_run.sh
@@ -3,7 +3,7 @@
 RECAPTCHA_SITE_KEY=$1
 RECAPTCHA_SECRET_KEY=$2
 
-sed -i "s/let recaptcha_site_key = .*/let recaptcha_site_key = $RECAPTCHA_SITE_KEY/" app/templater/static/js/main.js
+sed -i "s/let recaptcha_site_key = .*/let recaptcha_site_key = \"$RECAPTCHA_SITE_KEY\"/" app/templater/static/js/main.js
 sed -i "s/recaptcha_key = .*/recaptcha_key = $RECAPTCHA_SECRET_KEY/" app/config.ini
 
 docker-compose build --no-cache
