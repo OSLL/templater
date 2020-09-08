98b0f9f744402c49c531328e1bb87d36c6abfc85
Mark Zaslavskiy <mark.zaslavskiy@gmail.com>
2020-09-08 18:10:59 +0300

    Update build_and_run.sh

--- build_and_run.sh
+++ build_and_run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-RECAPTCHA_SITE_KEY=
-RECAPTCHA_SECRET_KEY=
+RECAPTCHA_SITE_KEY=$1
+RECAPTCHA_SECRET_KEY=$2
 
 sed -i "s/let recaptcha_site_key = .*/let recaptcha_site_key = $RECAPTCHA_SITE_KEY/" app/templater/static/js/main.js
 sed -i "s/recaptcha_key = .*/recaptcha_key = $RECAPTCHA_SECRET_KEY/" app/config.ini
