b181ff397691b0c2486eeedad0290ad7015bdfba
Mark Zaslavskiy <mark.zaslavskiy@gmail.com>
2020-09-08 19:33:47 +0300

    Update build_and_run.sh

--- build_and_run.sh
+++ build_and_run.sh
@@ -6,8 +6,9 @@ RECAPTCHA_SECRET_KEY=$2
 sed -i "s/let recaptcha_site_key = .*/let recaptcha_site_key = $RECAPTCHA_SITE_KEY/" app/templater/static/js/main.js
 sed -i "s/recaptcha_key = .*/recaptcha_key = $RECAPTCHA_SECRET_KEY/" app/config.ini
 
-
-docker-compose build --no-cache 
+docker-compose build --no-cache
+docker-compose down
+docker-compose rm -f
 docker-compose up -d
 
 sleep 10s
