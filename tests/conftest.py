d389a7f66f0854762594bf832a7e4e274286ec06
D1mitrii <dyusmirnov@stud.etu.ru>
2026-05-05 22:15:43 +0300

    feat: update driver fixture + data combinations for tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,11 +1,14 @@
 import pytest
 from selenium import webdriver
 from selenium.webdriver.remote.file_detector import LocalFileDetector
+from pathlib import Path
+
+from utils.combinations_generator import CombinationGenerator
 
 GRID_URL = "http://selenium-hub:4444/wd/hub"
 BASE_URL = "http://web:5000"
 
-@pytest.fixture
+@pytest.fixture(scope="session")
 def driver():
     options = webdriver.ChromeOptions()
     options.add_argument("--headless=new")
@@ -22,6 +25,28 @@ def driver():
 
     driver.quit()
 
+@pytest.fixture(autouse=True)
+def clean_between_tests(driver, base_url):
+    driver.get(base_url)
+    driver.delete_all_cookies()
+    driver.execute_script("window.localStorage.clear();")
+    driver.execute_script("window.sessionStorage.clear();")
+    
+    yield
+    
+    driver.delete_all_cookies()
+    driver.execute_script("window.localStorage.clear();")
+    driver.execute_script("window.sessionStorage.clear();")
+
 @pytest.fixture
 def base_url():
     return BASE_URL
+
+def pytest_generate_tests(metafunc):
+    if 'test_data_combination' in metafunc.fixturenames:
+        combinations = CombinationGenerator.get_all_combinations()
+        metafunc.parametrize(
+            'test_data_combination',
+            combinations,
+            ids=[f"{Path(t).suffix[1:]}_{Path(d).suffix[1:]}" for t, d in combinations]
+        )
